7de260cb7d38bd0f2155695996b3aa3340d6d734
merihcavdar <cavdarmerih@hotmail.com>
2023-11-02 15:11:36 +0300

    stepping ahead with epub_view

---
+++ lib/screens/epub_read_aloud.dart
@@ -0,0 +1,284 @@
+import 'package:html/parser.dart';
+import 'package:flutter/material.dart';
+import 'package:epub_view/epub_view.dart';
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter_tts/flutter_tts.dart';
+
+class EpubReadAloud extends StatefulWidget {
+  const EpubReadAloud({super.key});
+
+  @override
+  State<EpubReadAloud> createState() => _EpubReadAloudState();
+}
+
+enum TtsState { playing, stopped, paused, continued }
+
+class _EpubReadAloudState extends State<EpubReadAloud> {
+  late FlutterTts flutterTts;
+  String? language;
+  String? engine;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.5;
+  bool isCurrentLanguageInstalled = false;
+
+  String? _newVoiceText;
+  int? _inputLength;
+
+  TtsState ttsState = TtsState.stopped;
+
+  get isPlaying => ttsState == TtsState.playing;
+  get isStopped => ttsState == TtsState.stopped;
+  get isPaused => ttsState == TtsState.paused;
+  get isContinued => ttsState == TtsState.continued;
+
+  bool get isIOS => !kIsWeb && Platform.isIOS;
+  bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  bool get isWindows => !kIsWeb && Platform.isWindows;
+  bool get isWeb => kIsWeb;
+
+  late EpubController _epubReaderController;
+
+  @override
+  void initState() {
+    initTts();
+    _epubReaderController = EpubController(
+      document:
+          // EpubDocument.openAsset('assets/New-Findings-on-Shirdi-Sai-Baba.epub'),
+          EpubDocument.openAsset('assets/epubs/hukuk_tkr_2023.epub'),
+      epubCfi: 'epubcfi(/6/0[null]!/4/64)',
+
+      //     'epubcfi(/6/26[id4]!/4/2/2[id4]/22)', // book.epub Chapter 3 paragraph 10
+      // epubCfi:
+      //     'epubcfi(/6/6[chapter-2]!/4/2/1612)', // book_2.epub Chapter 16 paragraph 3
+    );
+    super.initState();
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    _setAwaitOptions();
+
+    if (isAndroid) {
+      _getDefaultEngine();
+      _getDefaultVoice();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    if (isAndroid) {
+      flutterTts.setInitHandler(() {
+        setState(() {
+          print("TTS Initialized");
+        });
+      });
+    }
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setPauseHandler(() {
+      setState(() {
+        print("Paused");
+        ttsState = TtsState.paused;
+      });
+    });
+
+    flutterTts.setContinueHandler(() {
+      setState(() {
+        print("Continued");
+        ttsState = TtsState.continued;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future<dynamic> _getLanguages() async => await flutterTts.getLanguages;
+
+  Future<dynamic> _getEngines() async => await flutterTts.getEngines;
+
+  Future _getDefaultEngine() async {
+    var engine = await flutterTts.getDefaultEngine;
+    if (engine != null) {
+      print(engine);
+    }
+  }
+
+  Future _getDefaultVoice() async {
+    var voice = await flutterTts.getDefaultVoice;
+    if (voice != null) {
+      print(voice);
+    }
+  }
+
+  Future _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText!.isNotEmpty) {
+        await flutterTts.speak(_newVoiceText!);
+      }
+    }
+  }
+
+  Future _setAwaitOptions() async {
+    await flutterTts.awaitSpeakCompletion(true);
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  Future _pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) setState(() => ttsState = TtsState.paused);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+    _epubReaderController.dispose();
+  }
+
+  List<DropdownMenuItem<String>> getEnginesDropDownMenuItems(dynamic engines) {
+    var items = <DropdownMenuItem<String>>[];
+    for (dynamic type in engines) {
+      items.add(DropdownMenuItem(
+          value: type as String?, child: Text(type as String)));
+    }
+    return items;
+  }
+
+  void changedEnginesDropDownItem(String? selectedEngine) async {
+    await flutterTts.setEngine(selectedEngine!);
+    language = null;
+    setState(() {
+      engine = selectedEngine;
+    });
+  }
+
+  List<DropdownMenuItem<String>> getLanguageDropDownMenuItems(
+      dynamic languages) {
+    var items = <DropdownMenuItem<String>>[];
+    for (dynamic type in languages) {
+      items.add(DropdownMenuItem(
+          value: type as String?, child: Text(type as String)));
+    }
+    return items;
+  }
+
+  void changedLanguageDropDownItem(String? selectedType) {
+    setState(() {
+      language = selectedType;
+      flutterTts.setLanguage(language!);
+      if (isAndroid) {
+        flutterTts
+            .isLanguageInstalled(language!)
+            .then((value) => isCurrentLanguageInstalled = (value as bool));
+      }
+    });
+  }
+
+  void _onChange(String text) {
+    setState(() {
+      _newVoiceText = text;
+    });
+  }
+
+  String _parseHtmlString(String? htmlString) {
+    final document = parse(htmlString);
+    final String parsedString =
+        parse(document.body?.text).documentElement!.text;
+
+    return parsedString;
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: EpubViewActualChapter(
+            controller: _epubReaderController,
+            builder: (chapterValue) => Text(
+              chapterValue?.chapter?.Title?.replaceAll('\n', '').trim() ?? '',
+              textAlign: TextAlign.start,
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.headphones),
+              color: Colors.white,
+              onPressed: () async {
+                String htmlContent = _parseHtmlString(_epubReaderController
+                        .currentValue?.chapter?.HtmlContent)
+                    .trim();
+                print(htmlContent);
+                print(_epubReaderController.generateEpubCfi());
+                await flutterTts.speak(htmlContent);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.save_alt),
+              color: Colors.white,
+              onPressed: () => _showCurrentEpubCfi(context),
+            ),
+          ],
+        ),
+        drawer: Drawer(
+          child: EpubViewTableOfContents(controller: _epubReaderController),
+        ),
+        body: EpubView(
+          builders: EpubViewBuilders<DefaultBuilderOptions>(
+            options: const DefaultBuilderOptions(),
+            chapterDividerBuilder: (_) => const Divider(),
+          ),
+          controller: _epubReaderController,
+        ),
+      );
+
+  void _showCurrentEpubCfi(context) {
+    final cfi = _epubReaderController.generateEpubCfi();
+
+    if (cfi != null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(cfi),
+          action: SnackBarAction(
+            label: 'GO',
+            onPressed: () {
+              _epubReaderController.gotoEpubCfi(cfi);
+            },
+          ),
+        ),
+      );
+    }
+  }
+}
--- lib/screens/read_page.dart
+++ lib/screens/read_page.dart
@@ -1,9 +1,7 @@
-import 'dart:io';
+import 'package:biri_okusun/screens/epub_read_aloud.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' as path;
-import 'package:epubx/epubx.dart';
-
-late EpubBook epubBook;
+import 'package:epub_view/epub_view.dart';
+import 'package:flutter/services.dart' show SystemChrome, SystemUiOverlayStyle;
 
 class ReadPage extends StatefulWidget {
   const ReadPage({super.key});
@@ -14,123 +12,51 @@ class ReadPage extends StatefulWidget {
 
 enum TtsState { playing, stopped, paused, continued }
 
-class _ReadPageState extends State<ReadPage> {
-  bool loading = false;
-  String filePath = "";
-
-  void loadFile() async {
-    String fileName = 'assets/epubs/hukuk_tkr_2023.epub';
-    String fullPath = path.join(Directory.current.path, fileName);
-    var targetFile = File(fullPath);
-    List<int> bytes = await targetFile.readAsBytes();
-
-    epubBook = await EpubReader.readBook(bytes);
-
-// COMMON PROPERTIES
-
-// Book's title
-    String? title = epubBook.Title;
-
-// Book's authors (comma separated list)
-    String? author = epubBook.Author;
-
-// Book's authors (list of authors names)
-    List<String?>? authors = epubBook.AuthorList;
-
-// Book's cover image (null if there is no cover)
-
-// CHAPTERS
-
-// Enumerating chapters
-
-    epubBook.Chapters?.forEach((EpubChapter chapter) {
-      // Title of chapter
-      String? chapterTitle = chapter.Title;
-
-      // HTML content of current chapter
-      String? chapterHtmlContent = chapter.HtmlContent;
-
-      // Nested chapters
-      List<EpubChapter?>? subChapters = chapter.SubChapters;
-    });
-
-// CONTENT
-
-// Book's content (HTML files, stylesheets, images, fonts, etc.)
-    EpubContent? bookContent = epubBook.Content;
-
-// IMAGES
-
-// All images in the book (file name is the key)
-    Map<String, EpubByteContentFile>? images = bookContent?.Images;
-
-    EpubByteContentFile? firstImage = images?.values.first;
-
-// Content type (e.g. EpubContentType.IMAGE_JPEG, EpubContentType.IMAGE_PNG)
-    EpubContentType? contentType = firstImage?.ContentType;
-
-// MIME type (e.g. "image/jpeg", "image/png")
-    String? mimeContentType = firstImage?.ContentMimeType;
-
-// HTML & CSS
-
-// All XHTML files in the book (file name is the key)
-    Map<String, EpubTextContentFile>? htmlFiles = bookContent?.Html;
-
-// All CSS files in the book (file name is the key)
-    Map<String, EpubTextContentFile>? cssFiles = bookContent?.Css;
-
-// Entire HTML content of the book
-    htmlFiles?.values.forEach((EpubTextContentFile htmlFile) {
-      String? htmlContent = htmlFile.Content;
-    });
-
-// All CSS content in the book
-    cssFiles?.values.forEach((EpubTextContentFile cssFile) {
-      String? cssContent = cssFile.Content;
-    });
-
-// OTHER CONTENT
-
-// All fonts in the book (file name is the key)
-    Map<String, EpubByteContentFile>? fonts = bookContent?.Fonts;
-
-// All files in the book (including HTML, CSS, images, fonts, and other types of files)
-    Map<String, EpubContentFile>? allFiles = bookContent?.AllFiles;
-
-// ACCESSING RAW SCHEMA INFORMATION
-
-// EPUB OPF data
-    EpubPackage? package = epubBook.Schema?.Package;
-
-// EPUB NCX data
-    EpubNavigation? navigation = epubBook.Schema?.Navigation;
-
-// Enumerating NCX metadata
-    navigation?.Head?.Metadata?.forEach((EpubNavigationHeadMeta meta) {
-      String? metadataItemName = meta.Name;
-      String? metadataItemContent = meta.Content;
-    });
+class _ReadPageState extends State<ReadPage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
   }
 
   @override
-  void initState() {
-    super.initState();
-    loadFile();
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    _setSystemUIOverlayStyle();
+  }
+
+  Brightness get platformBrightness => MediaQueryData.fromView(
+          WidgetsBinding.instance.platformDispatcher.views.single)
+      .platformBrightness;
+  void _setSystemUIOverlayStyle() {
+    if (platformBrightness == Brightness.light) {
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: Colors.grey[50],
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ));
+    } else {
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Colors.grey[850],
+        systemNavigationBarIconBrightness: Brightness.light,
+      ));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Biri Okusun'),
-        ),
-        body: Center(
-          child: 
-          ),
-        ),
+        body: EpubReadAloud(),
       ),
     );
   }
