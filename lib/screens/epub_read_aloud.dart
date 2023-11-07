05beae0534c84528e923e813bfe0e80e844663f3
merihcavdar <cavdarmerih@hotmail.com>
2023-11-07 18:01:27 +0300

    epub_parser package initilazing

--- lib/screens/epub_read_aloud.dart
+++ lib/screens/epub_read_aloud.dart
@@ -1,8 +1,11 @@
-import 'package:html/parser.dart';
-import 'package:flutter/material.dart';
-import 'package:epub_view/epub_view.dart';
+import 'dart:io' as io;
 import 'dart:async';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:epub_parser/epub_parser.dart' as eparser;
+import 'package:path/path.dart' as path;
+import 'package:flutter/material.dart';
+
+var coverImage;
 
 class EpubReadAloud extends StatefulWidget {
   const EpubReadAloud({super.key});
@@ -14,13 +17,11 @@ class EpubReadAloud extends StatefulWidget {
 enum TtsState { playing, stopped, paused, continued }
 
 class _EpubReadAloudState extends State<EpubReadAloud> {
-  late EpubController _epubReaderController;
-
   FlutterTts flutterTts = FlutterTts();
 
   Future<void> configureTts() async {
     await flutterTts.setLanguage('tr-TR');
-    await flutterTts.setSpeechRate(1.5);
+    await flutterTts.setSpeechRate(1.0);
     await flutterTts.setVolume(1.0);
   }
 
@@ -35,94 +36,85 @@ class _EpubReadAloudState extends State<EpubReadAloud> {
   @override
   void initState() {
     configureTts();
-    _epubReaderController = EpubController(
-      document: EpubDocument.openAsset('assets/epubs/ithalat-ihracat.epub'),
-    );
+    loadEPub();
     super.initState();
   }
 
   @override
   void dispose() {
     super.dispose();
-    _epubReaderController.dispose();
   }
 
-  String _parseHtmlString(String? htmlString) {
-    final document = parse(htmlString);
-    final String parsedString =
-        parse(document.body?.text).documentElement!.text;
+  void loadEPub() async {
+    String fileName = '/assets/epubs/kredi_yeni.epub';
+    String fullPath = path.join(io.Directory.current.path, fileName);
+    var targetFile = io.File(fullPath);
+    List<int> bytes = await targetFile.readAsBytes();
+    eparser.EpubBook epubBook = await eparser.EpubReader.readBook(bytes);
+    String? title = epubBook.Title;
+    String? author = epubBook.Author;
 
-    return parsedString;
+// Enumerating chapters
+    epubBook.Chapters?.forEach((eparser.EpubChapter chapter) {
+      // Title of chapter
+      String? chapterTitle = chapter.Title;
+
+      // HTML content of current chapter
+      String? chapterHtmlContent = chapter.HtmlContent;
+
+      // Nested chapters
+      List<eparser.EpubChapter>? subChapters = chapter.SubChapters;
+      print(chapterTitle);
+      print(chapterHtmlContent);
+      print(subChapters);
+    });
+
+// CONTENT
+
+// Book's content (HTML files, stylesheets, images, fonts, etc.)
+    eparser.EpubContent? bookContent = epubBook.Content;
+
+// HTML & CSS
+
+// All XHTML files in the book (file name is the key)
+    Map<String, eparser.EpubTextContentFile>? htmlFiles = bookContent?.Html;
+
+// Entire HTML content of the book
+    htmlFiles?.values.forEach((eparser.EpubTextContentFile htmlFile) {
+      String? htmlContent = htmlFile.Content;
+    });
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: EpubViewActualChapter(
-            controller: _epubReaderController,
-            builder: (chapterValue) => Text(
-              chapterValue?.chapter?.Title?.replaceAll('\n', '').trim() ?? '',
-              textAlign: TextAlign.start,
-            ),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.headphones),
-              color: Colors.white,
-              onPressed: () async {
-                String htmlContent = _parseHtmlString(_epubReaderController
-                        .currentValue?.chapter?.HtmlContent)
-                    .trim();
-                print(htmlContent);
-
-                speakText(htmlContent);
-                //print(_epubReaderController.currentValue?.chapter?.Title);
-                /* for (var i in _epubReaderController.tableOfContents()) {
+      appBar: AppBar(
+        title: const Text("hello"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.headphones),
+            color: Colors.white,
+            onPressed: () async {
+              //String htmlContent = _parseHtmlString().trim();
+              speakText("ben merih");
+              //print(_epubReaderController.currentValue?.chapter?.Title);
+              /* for (var i in _epubReaderController.tableOfContents()) {
                   print(i.title);
                   print(i.startIndex);
                 }
                 */
 
-                //print(_epubReaderController.generateEpubCfi());
-                //_epubReaderController
-                //     .gotoEpubCfi('epubcfi(/6/22[chapter05]!/4/2)');
-              },
-            ),
-            IconButton(
-                icon: const Icon(Icons.save_alt),
-                color: Colors.white,
-                onPressed: () {
-                  _showCurrentEpubCfi(context);
-                }),
-          ],
-        ),
-        drawer: Drawer(
-          child: EpubViewTableOfContents(controller: _epubReaderController),
-        ),
-        body: EpubView(
-          builders: EpubViewBuilders<DefaultBuilderOptions>(
-            options: const DefaultBuilderOptions(),
-            chapterDividerBuilder: (_) => const Divider(),
-          ),
-          controller: _epubReaderController,
-        ),
-      );
-
-  void _showCurrentEpubCfi(context) {
-    final cfi = _epubReaderController.generateEpubCfi();
-
-    if (cfi != null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(cfi),
-          action: SnackBarAction(
-            label: 'GO',
-            onPressed: () {
-              _epubReaderController.gotoEpubCfi(cfi);
+              //print(_epubReaderController.generateEpubCfi());
+              //_epubReaderController
+              //     .gotoEpubCfi('epubcfi(/6/22[chapter05]!/4/2)');
             },
           ),
-        ),
-      );
-    }
-  }
+          IconButton(
+              icon: const Icon(Icons.save_alt),
+              color: Colors.white,
+              onPressed: () {}),
+        ],
+      ),
+      body: Center(
+        child: Image(image: coverImage),
+      ));
 }
